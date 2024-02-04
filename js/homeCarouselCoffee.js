019414f44849086ffbf8f336ec6040ccc42872c6
GitAlBo93 <a.l.e.k.s.93@mail.ru>
2024-02-04 17:28:34 +0500

    добавлена папка с примерами. поломаны кнопки карусели

--- js/homeCarouselCoffee.js
+++ js/homeCarouselCoffee.js
@@ -1,65 +1,77 @@
 const ButtonLeft = document.querySelector("#ButtonLeftCarousel");
 const ButtonRight = document.querySelector("#ButtonRightCarousel");
+const ContFCM = document.querySelector('.ContFCMenuCarousel');
 const FCMenu1 = document.querySelector("#FCMenu1");
 const FCMenu2 = document.querySelector("#FCMenu2");
 const FCMenu3 = document.querySelector("#FCMenu3");
 const FCLine1 = document.querySelector(".FCLine_1");
 const FCLine2 = document.querySelector(".FCLine_2");
 const FCLine3 = document.querySelector(".FCLine_3");
-let position = 0;
+let position=0;
 
+// let style = getComputedStyle(FCMenu1);
+// let position = style.right;
+// console.log(position);
 
 
 ButtonLeft.addEventListener('click',functionLeftClick);
 ButtonRight.addEventListener('click',functionRightClick);
 
 function functionLeftClick(){
-    position=position+100;
-        if (position>=0) {
-            FCMenu1.style.right = 0 + '%';
-            FCMenu2.style.right = -100 + '%';
-            FCMenu3.style.right = -200 + '%';
-        }
-        if (position==100) {
-            FCMenu1.style.right = 100 + '%';
-            FCMenu2.style.right = 0 + '%';
-            FCMenu3.style.right = -100 + '%';
-        }
-        if (position==200) {
-            FCMenu1.style.right = 200 + '%';
-            FCMenu2.style.right = 100 + '%';
-            FCMenu3.style.right = 0 + '%';
-        }
-        if (position>200) {
-            position=0;
-
-        }
-    console.log (FCMenu1.style.right);
+    position+=60;
+    ContFCM.style.right = position + '%';
 }
 
-function functionRightClick(){
-    position=position-100;
-        if (position<=0) {
-            FCMenu1.style.right = 0 + '%';
-            FCMenu2.style.right = 100 + '%';
-            FCMenu3.style.right = 200 + '%';
-        }
-        if (position==-100) {
-            FCMenu1.style.right = -100 + '%';
-            FCMenu2.style.right = 0 + '%';
-            FCMenu3.style.right = 100 + '%';
-        }
-        if (position==-200) {
-            FCMenu1.style.right = -200 + '%';
-            FCMenu2.style.right = -100 + '%';
-            FCMenu3.style.right = 0 + '%';
-        }
-        if (position<200) {
-            position=0;
+// function functionLeftClick(){
+    
+//     position=position+100;
+//         // if (position<=0) {
+//         //     FCMenu1.style.right = 0 + '%';
+//         //     FCMenu2.style.right = -100 + '%';
+//         //     FCMenu3.style.right = -200 + '%';
+//         // }
+//         // if (position==100) {
+//         //     FCMenu1.style.right = 100 + '%';
+//         //     FCMenu2.style.right = 0 + '%';
+//         //     FCMenu3.style.right = -100 + '%';
+//         // }
+//         // if (position==200) {
+//         //     FCMenu1.style.right = 200 + '%';
+//         //     FCMenu2.style.right = 100 + '%';
+//         //     FCMenu3.style.right = 0 + '%';
+//         // }
+//         // if (position>200) {
+//         //     position=0;
 
-        }
-    console.log (FCMenu1.style.right);
-}
+//         // }
+//         console.log(position);
+//     console.log (FCMenu1.style.right);
+// }
+
+// function functionRightClick(){
+//     position=position-100;
+        
+//             if (position>0) {
+//                 FCMenu1.style.right = 0 + '%';
+//                 FCMenu2.style.right = -100 + '%';
+//                 FCMenu3.style.right = -200 + '%';
+//             }
+//             if (position==100) {
+//                 FCMenu1.style.right = 100 + '%';
+//                 FCMenu2.style.right = 0 + '%';
+//                 FCMenu3.style.right = -100 + '%';
+//             }
+//             if (position==200) {
+//                 FCMenu1.style.right = 200 + '%';
+//                 FCMenu2.style.right = 100 + '%';
+//                 FCMenu3.style.right = 0 + '%';
+//         }
+//         // if (position<200) {
+//         //     position=0;
+
+//         // }
+//     console.log (FCMenu1.style.right);
+// }
 // function functionRightClick(){
 //     positionFCM2=positionFCM2-200;
 //     FCMenu2.style.right = positionFCM2 + '%';
