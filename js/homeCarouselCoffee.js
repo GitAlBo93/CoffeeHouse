da87f00b67db862e42aad71dfe7c3eba535ce97a
GitAlBo93 <a.l.e.k.s.93@mail.ru>
2024-02-04 14:12:46 +0500

    изменения: добавлен функционал карусели по кнопке влево. кнопка вправо  не готова

--- js/homeCarouselCoffee.js
+++ js/homeCarouselCoffee.js
@@ -3,28 +3,85 @@ const ButtonRight = document.querySelector("#ButtonRightCarousel");
 const FCMenu1 = document.querySelector("#FCMenu1");
 const FCMenu2 = document.querySelector("#FCMenu2");
 const FCMenu3 = document.querySelector("#FCMenu3");
+const FCLine1 = document.querySelector(".FCLine_1");
+const FCLine2 = document.querySelector(".FCLine_2");
+const FCLine3 = document.querySelector(".FCLine_3");
+let position = 0;
+
 
 
 ButtonLeft.addEventListener('click',functionLeftClick);
 ButtonRight.addEventListener('click',functionRightClick);
 
-function functionLeftClick() {
-    FCMenu2.style.setProperty("right","0%");
-    FCMenu2.classList.toggle("active");
-    console.log("ClickLeft");
-        let style = getComputedStyle(FCMenu3);
-        let styleRight = style.right;
-        if (styleRight != -1440) {
-           console.log("Значение:", styleRight); 
+function functionLeftClick(){
+    position=position+100;
+        if (position>=0) {
+            FCMenu1.style.right = 0 + '%';
+            FCMenu2.style.right = -100 + '%';
+            FCMenu3.style.right = -200 + '%';
         }
-        else console.log("Меньше -100ppxx");
+        if (position==100) {
+            FCMenu1.style.right = 100 + '%';
+            FCMenu2.style.right = 0 + '%';
+            FCMenu3.style.right = -100 + '%';
+        }
+        if (position==200) {
+            FCMenu1.style.right = 200 + '%';
+            FCMenu2.style.right = 100 + '%';
+            FCMenu3.style.right = 0 + '%';
+        }
+        if (position>200) {
+            position=0;
+
+        }
+    console.log (FCMenu1.style.right);
 }
 
-function functionRightClick() {
-    FCMenu2.style.setProperty("right","-200%");
-    FCMenu2.style.setProperty("transition","all 1s ease");
-    console.log("ClickRight");
-}
+function functionRightClick(){
+    position=position-100;
+        if (position<=0) {
+            FCMenu1.style.right = 0 + '%';
+            FCMenu2.style.right = 100 + '%';
+            FCMenu3.style.right = 200 + '%';
+        }
+        if (position==-100) {
+            FCMenu1.style.right = -100 + '%';
+            FCMenu2.style.right = 0 + '%';
+            FCMenu3.style.right = 100 + '%';
+        }
+        if (position==-200) {
+            FCMenu1.style.right = -200 + '%';
+            FCMenu2.style.right = -100 + '%';
+            FCMenu3.style.right = 0 + '%';
+        }
+        if (position<200) {
+            position=0;
 
-let style = getComputedStyle(FCMenu2);
-console.log(style.right);
+        }
+    console.log (FCMenu1.style.right);
+}
+// function functionRightClick(){
+//     positionFCM2=positionFCM2-200;
+//     FCMenu2.style.right = positionFCM2 + '%';
+//     console.log (positionFCM2);
+// }
+// function functionLeftClick() {
+//     FCMenu2.style.setProperty("right","0%");
+//     FCMenu2.classList.toggle("active");
+//     console.log("ClickLeft");
+//         let style = getComputedStyle(FCMenu3);
+//         let styleRight = style.right;
+//         if (styleRight != -1440) {
+//            console.log("Значение:", styleRight); 
+//         }
+//         else console.log("Меньше -100ppxx");
+// }
+
+// function functionRightClick() {
+//     FCMenu2.style.setProperty("right","-200%");
+//     FCMenu2.style.setProperty("transition","all 1s ease");
+//     console.log("ClickRight");
+// }
+
+// let style = getComputedStyle(FCMenu2);
+// console.log(style.right);
