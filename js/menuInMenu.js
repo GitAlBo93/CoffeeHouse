0348b9221878df5e2be872d8a7551633f8d448ac
GitAlBo93 <a.l.e.k.s.93@mail.ru>
2024-02-16 18:08:22 +0500

    feat: добавил переключение меню coffee/tea/dessert

--- js/menuInMenu.js
+++ js/menuInMenu.js
@@ -1,14 +1,33 @@
 const BtnMenuCoffee = document.querySelector (".BtnMenuCoffee");
 const BtnMenuTea = document.querySelector(".BtnMenuTea");
 const BtnMenuDessert = document.querySelector(".BtnMenuDessert");
-const menuCardsCoffee = document.querySelector("menuCardsCoffee");
-const menuCardsTea = document.querySelector("menuCardsTea");
-const menuCardsDessert = document.querySelector("menuCardsDessert");
+const menuCardsCoffee = document.querySelector(".menuCardsCoffee");
+const menuCardsTea = document.querySelector(".menuCardsTea");
+const menuCardsDessert = document.querySelector(".menuCardsDessert");
+const body = document.body;
 
+BtnMenuCoffee.addEventListener('click', functionClickCoffee);
 BtnMenuTea.addEventListener('click', functionClickTea);
 BtnMenuDessert.addEventListener('click', functionClickDessert);
 
+
+function functionClickCoffee() {
+    menuCardsCoffee.classList.add("activ");
+    menuCardsTea.classList.remove("activ");
+    menuCardsDessert.classList.remove("activ");
+    
+}
+
 function functionClickTea() {
-    menuCardsTea.classList.toggle("active");
+    menuCardsCoffee.classList.remove("activ");
+    menuCardsTea.classList.add("activ");
+    menuCardsDessert.classList.remove("activ");
+    
+}
+
+function functionClickDessert() {
+    menuCardsCoffee.classList.remove("activ");
+    menuCardsTea.classList.remove("activ");
+    menuCardsDessert.classList.add("activ");
     
 }
