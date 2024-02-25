fa8bc0ccb4477b308b0e62dee3f194b3f0dd82eb
Alex Bog <a.l.e.k.s.93@mail.ru>
2024-02-25 20:32:43 +0500

    fix: попробовал добавить описание товара и добавки в разделе кофе

--- js/modalCard.js
+++ js/modalCard.js
@@ -10,17 +10,8 @@ async function go (){
       let card;
       document.getElementById("modalBtnClose").addEventListener('click', clickCloseModal);
       function clickCloseModal() {
-          document.getElementById("modal").classList.remove("open")
+      document.getElementById("modal").classList.remove("open")
       }
-
-      // document.getElementById("Card1Coffee").addEventListener('click', clickCard1Coffee);
-      // document.getElementById("Card2Coffee").addEventListener('click', clickCard2Coffee);
-      // document.getElementById("Card3Coffee").addEventListener('click', clickCard3Coffee);
-      // document.getElementById("Card4Coffee").addEventListener('click', clickCard4Coffee);
-      // document.getElementById("Card5Coffee").addEventListener('click', clickCard5Coffee);
-      // document.getElementById("Card6Coffee").addEventListener('click', clickCard6Coffee);
-      // document.getElementById("Card7Coffee").addEventListener('click', clickCard7Coffee);
-      // document.getElementById("Card8Coffee").addEventListener('click', clickCard8Coffee);
 
       function ClickCoffeeCard(index) {
         return function() {
@@ -35,45 +26,13 @@ async function go (){
         card.addEventListener('click', ClickCoffeeCard(i - 1));
       }
 
-      
-      // function clickCard1Coffee() {
-      //   CardOpen = 0;
-      //   renderModal();
-      //  }
-      // function clickCard2Coffee() {
-      //   CardOpen = 1;
-      //   renderModal();
-      // }
-      // function clickCard3Coffee() {
-      //   CardOpen = 2;
-      //   renderModal();
-      // }
-      // function clickCard4Coffee() {
-      //   CardOpen = 3;
-      //   renderModal();
-
-      // }
-      // function clickCard5Coffee() {
-      //   CardOpen = 4;
-      //   renderModal();
-      // }
-      // function clickCard6Coffee() {
-      //   CardOpen = 5;
-      //   renderModal();
-      // }
-      // function clickCard7Coffee() {
-      //   CardOpen = 6;
-      //   renderModal();
-      // }
-      // function clickCard8Coffee() {
-      //   CardOpen = 7;
-      //   renderModal();
-      // }
-    
     function renderModal (){
       card = respjson[CardOpen];
       document.getElementById("modal").classList.add("open");
       document.getElementById("price").innerHTML = "$" + card.price;
+      document.getElementById("name").innerHTML = card.name;
+      document.getElementById("description").innerHTML = card.description;
+      // document.getElementById("name").innerHTML = card.additives[0].name;
     } 
   
 }
